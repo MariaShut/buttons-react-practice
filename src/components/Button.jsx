487f7cd8b34988c288ba00dc1f61030c050e7e9b
Maria <73496077+MariaShut@users.noreply.github.com>
2024-12-12 00:48:47 +0300

    initial

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line react/prop-types
+export const Button = ({ children, className, mode = "filled", Icon, ...props }) => {
+  let cssClasses = `button ${mode}-button`;
+
+  if (Icon) {
+    cssClasses += ' icon-button';
+  }
+
+  if (className) {
+    cssClasses += ' ' + className;
+  }
+
+  return (
+    <button className={cssClasses} {...props}>
+      {Icon && (
+        <span className="button-icon">
+          <Icon />
+        </span>
+      )}
+      <span>{children}</span>
+    </button>
+  )
+}
